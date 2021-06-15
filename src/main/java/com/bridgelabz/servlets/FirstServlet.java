eb54f1515cb3f1230e386e27df47ae23a03b946d
Shruti Verma <shrutiverma3105@gmail.com>
2021-06-15 13:54:27 +0530

    UC1: my first servlet

---
+++ src/main/java/com/bridgelabz/servlets/FirstServlet.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;  
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h1 fontcolor:red>My First Servlet!</h1>");
+		out.close();
+	}
+}
