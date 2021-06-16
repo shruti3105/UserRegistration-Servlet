d2eb42b0fc6f2a261e6edd0c03229989e7ba58b6
Shruti Verma <shrutiverma3105@gmail.com>
2021-06-16 10:52:15 +0530

    UC3: name validation

--- src/main/java/com/bridgelabz/servlets/LoginServlet.java
+++ src/main/java/com/bridgelabz/servlets/LoginServlet.java
@@ -25,15 +25,16 @@ public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String user = request.getParameter("user");
 		String pwd = request.getParameter("pwd");
-		String userID = getServletConfig().getInitParameter("user");
-		String password = getServletConfig().getInitParameter("password");
-		if(userID.equals(user) && password.equals(pwd)) {
+		String validFirstName = "^[A-Z]{1}[a-z]{2,}";
+		Pattern pattern = Pattern.compile(validFirstName);
+		Matcher match = pattern.matcher(user);
+		if(match.matches()) {
 			request.setAttribute("user", user);
 			request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
 		}else {
 			RequestDispatcher rd = getServletContext().getRequestDispatcher("/Login.html");
 			PrintWriter out = response.getWriter();
-			out.println("<font color=red>Either user name or password is wrong</font>");
+			out.println("<font color=red>Incorrect Name , Please Enter The Correct Name</font>");
 			rd.include(request, response);
 		}
 	}
